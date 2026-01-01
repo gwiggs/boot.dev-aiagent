32eac87b4a1a2fb15487c42dae8283ecc0d314f5
greg <greg.wiggs@proton.me>
2026-01-02 07:24:52 +1030

    added arg parsing so that the user can enter their prompt in the cli.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError(
@@ -11,8 +13,24 @@ if api_key is None:
 
 def main():
     client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        model='gemini-2.5-flash', contents=args.user_prompt)
+
+    if response.usage_metadata is None:
+        raise RuntimeError(
+            "API request failed. Check internet connection, and API key.")
+
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(
+        f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 
