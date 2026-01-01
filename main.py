72dec411bcb642f87a821c7b55b9130e49cc734b
greg <greg.wiggs@proton.me>
2026-01-02 08:17:01 +1030

    add the test program: calculator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 load_dotenv()
@@ -16,20 +17,25 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Enable verbose output")
+
     args = parser.parse_args()
     # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+    messages = [types.Content(
+        role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.user_prompt)
+        model='gemini-2.5-flash', contents=messages)
 
     if response.usage_metadata is None:
         raise RuntimeError(
             "API request failed. Check internet connection, and API key.")
-
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(
-        f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(
+            f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
